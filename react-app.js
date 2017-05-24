edc48061c0de5d8abd9be5dd4012a880896062c9
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-24 15:23:43 -0400

    Adds text div to page which responds to an input by dynamically updating to display whatever the user types in.

--- react-app.js
+++ react-app.js
@@ -19,11 +19,33 @@ class Message extends React.Component {
 
     constructor(props) {
         super(props);   // props for properties - call it what you want, contains the arguments/parameters
-        this.text = props.text;
+        // this.text = props.text;
+        this.state = {
+            text: this.props.text
+        }
+
+        this.changeMessage = this.changeMessage.bind(this);
+        // necessary so that this. within changeMessage refers to the instance of this class.
     }
 
+    // this. refers to the instance of this class....
+
     render() {
-        return <div>{this.text}</div>
+        return  <div>
+                    <div className="message">{this.state.text}</div>
+                    <input onChange={this.changeMessage} type="text" value={this.state.text} />
+                </div>
+    }
+    // in the HTML in the return in render(),
+    // use className= to set the classes.
+    // use onChange to addEventListener to that object.
+
+
+    changeMessage(event) {
+        // event listener so it takes an event argument!
+        this.setState({text: event.target.value})
+        // this.setState() is inherited from the React.Component class.
+        // references the this.state object set up in the constructor function.
     }
 
 }
