eed0ac50ca17783f59edf36d7791c9bd5019ef31
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-24 16:56:26 -0400

    Adds submit button which saves state.title and state.content to a state.post object and displays the post below the input fields.

--- post-react.js
+++ post-react.js
@@ -2,21 +2,35 @@ class Post extends React.Component {
 
     constructor(props) {
         super(props);
-        // this.state = {
-        //     title: this.props.title,
-        //     content: this.props.content
-        // };
+        this.state = {
+            post: {
+                title: "",
+                content: ""
+            }
+        };
         this.updateTitle = this.updateTitle.bind(this);
         this.updateContent = this.updateContent.bind(this);
+        this.updatePost = this.updatePost.bind(this);
     }
 
     render() {
-        return  <div className="post-input">
-                    <div className="post-input__title">
-                        <input onChange={this.updateTitle} type="text" placeholder="Post title"/>
+        return  <div className="post-module">
+                    <div className="post-input">
+                        <div className="post-input__title">
+                            <input onChange={this.updateTitle} type="text" placeholder="Post title"/>
+                        </div>
+                        <div className="post-input__content">
+                            <textarea onChange={this.updateContent} type="text" placeholder="Type your post content here."></textarea>
+                        </div>
+                        <button onClick={this.updatePost} className="post-input__submit">Update post</button>
                     </div>
-                    <div className="post-input__content">
-                        <textarea onChange={this.updateContent} type="text" placeholder="Type your post content here."></textarea>
+                    <div className="post-display">
+                        <div className="post-display__title">
+                            {this.state.post.title}
+                        </div>
+                        <div className="post-display__content">
+                            {this.state.post.content}
+                        </div>
                     </div>
                 </div>
     }
@@ -30,6 +44,15 @@ class Post extends React.Component {
     updateContent(event) {
         this.setState({
             content: event.target.value
+        });
+    }
+
+    updatePost(event) {
+        this.setState({
+            post: {
+                title: this.state.title,
+                content: this.state.content
+            }
         });
     }
 
