23b534ccb30884404cea77dd7f48bd8d8ae85766
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-24 14:27:27 -0400

    Adds Message class which takes text parameter and prints it to page.

--- react-app.js
+++ react-app.js
@@ -11,6 +11,24 @@ class HelloWorld extends React.Component {
 }
 
 ReactDOM.render(
-    <HelloWorld />, //what we want to render, can use
+    <HelloWorld />, //what we want to render, note use of ending slash!
     document.getElementById('react') //where we want it to render -- a DOM object as you're used to!
+)
+
+class Message extends React.Component {
+
+    constructor(props) {
+        super(props);   // props for properties - call it what you want, contains the arguments/parameters
+        this.text = props.text;
+    }
+
+    render() {
+        return <div>{this.text}</div>
+    }
+
+}
+
+ReactDOM.render(
+    <Message text="Welcome to my site." />,
+    document.getElementById('react')
 )
