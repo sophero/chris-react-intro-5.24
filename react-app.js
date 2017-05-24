dacff305700b2f365686ad184ec908d2048bee82
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-24 14:09:45 -0400

    Gets Hello World! printing to page sing ReactDOM.reder on the HelloWorld class, also fixes script tag in html body to reference jsx.

--- react-app.js
+++ react-app.js
@@ -5,7 +5,12 @@ class HelloWorld extends React.Component {
     }
 
     render() {
-        
+        return <div>Hello World!</div> // note no string quptation marks!
     }
 
 }
+
+ReactDOM.render(
+    <HelloWorld />, //what we want to render, can use
+    document.getElementById('react') //where we want it to render -- a DOM object as you're used to!
+)
